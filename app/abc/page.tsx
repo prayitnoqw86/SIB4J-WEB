9eefbc237b00d4861dea63784c05cf31b895b6ce
Teguh Prayitno <prayitnoqw86@gmail.com>
2026-04-27 15:09:13 +0800

    Tugas Kuliah Push Your Next JS to Github Repository

---
+++ app/abc/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog () {
+    return (
+        <div>
+        <p> Ini adalah halaman blog Teguh Prayitno !! </p>
+        <input type="text" placeholder="Masukkan teks disini" />
+        <button> Click Saya </button>
+        </div>
+    );
+    }
---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+
+export default function learn () {
+    return (
+     
+        <div>
+            <div><img src="https://www.google.com/imgres?q=desain&imgurl=https%3A%2F%2Fidseducation.com%2Fwp-content%2Fuploads%2F2022%2F11%2FIlustrasi-pada-Desain-Grafis.jpg&imgrefurl=https%3A%2F%2Fidseducation.com%2Farticle%2Fritme-dalam-desain-grafis%2F&docid=b7TiMvfN2uShrM&tbnid=KNWckMpLKwdUCM&vet=12ahUKEwjJqLnrzIGUAxXGaHADHXryBFMQnPAOegQIKBAB..i&w=1000&h=600&hcb=2&ved=2ahUKEwjJqLnrzIGUAxXGaHADHXryBFMQnPAOegQIKBAB"/>
+            <p> Design</p>
+            </div>
+
+            <div> <h1>FIGMA</h1>
+            <p> 8 Hours a go</p>
+            </div>
+        </div>
+    )
+    }
